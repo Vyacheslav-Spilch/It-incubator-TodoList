4cd6d94927bf70f0ed35b830b773744d31c6fe72
Vyacheslav-Spilch <spil4evsky@yandex.ru>
2024-04-08 20:47:22 +0300

    creating a formik object

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -8,7 +8,7 @@ import { useAppSelector } from './state/store';
 import LinearProgress from '@mui/material/LinearProgress';
 import { RequestStatusType } from './state/app-reducer';
 import  CustomizedSnackBars  from './components/ErrorSnackbar/ErrorSnackbar';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { TodolistList } from './features/TodolistList/TodolistList';
 import { Login } from './components/Login/Login';
 
@@ -21,11 +21,13 @@ return (
     <div className="App">
         <CustomizedSnackBars />
         <ButtonAppBar />
-        {status === 'loading' && <LinearProgress color="primary" />}
+        {status === 'loading' && <LinearProgress color="primary"/>}
         <Container fixed>
             <Routes>
                 <Route path={'/'} element={<TodolistList/>}/>
                 <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/404'} element={<h2 style={{ alignItems: 'center' }}>PAGE NOT FOUND</h2>}/>
+                <Route path={'*'} element={<Navigate to={'/404'}/>}/>
             </Routes>
         </Container>
     </div>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -9,8 +9,40 @@ import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import { useAppDispatch } from '../../state/store'
 import { setAppStatusAC } from '../../state/app-reducer'
+import { useFormik } from 'formik'
+
+type ErrorsType = {
+    email?: string 
+    password?: string
+    rememberMe?: string
+}
 
 export const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: ErrorsType = {}
+            if(!values.email) {
+                errors.email = 'Requered'
+            } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Incorrect email address'
+            }
+            if(!values.password) {
+                errors.password = 'Requered'
+            } else if(values.password.length < 4) {
+                errors.password = 'Should be mode 3 symbols'
+            }
+            return errors
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2))
+        },
+    })
 
     let dispatch = useAppDispatch()
     useEffect(() => {
@@ -20,27 +52,57 @@ export const Login = () => {
     return (
     <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
-            <FormControl>
-                <FormLabel>
-                <p>
-                    To log in get registered
-                    <a href={'https://social-network.samuraijs.com/'} target={'_blank'}>
-                    here
-                    </a>
-                </p>
-                <p>or use common test account credentials:</p>
-                <p>Email: free@samuraijs.com</p>
-                <p>Password: free</p>
-                </FormLabel>
-                <FormGroup>
-                <TextField label="Email" margin="normal" />
-                <TextField type="password" label="Password" margin="normal" />
-                <FormControlLabel label={'Remember me'} control={<Checkbox />} />
-                <Button type={'submit'} variant={'contained'} color={'primary'}>
-                    Login
-                </Button>
-                </FormGroup>
-            </FormControl>
+            <form onSubmit={formik.handleSubmit}>
+                <FormControl>
+                    <FormLabel>
+                    <p>
+                        To log in get registered
+                        <a href={'https://social-network.samuraijs.com/'} target={'_blank'}>
+                        here
+                        </a>
+                    </p>
+                    <p>or use common test account credentials:</p>
+                    <p>Email: free@samuraijs.com</p>
+                    <p>Password: free</p>
+                    </FormLabel>
+                    <FormGroup>
+                    <TextField 
+                        label="Email" 
+                        margin="normal" 
+                        name='email'
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                    {formik.errors.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+                    <TextField 
+                        type="password" 
+                        label="Password"
+                        name='password'
+                        onChange={formik.handleChange}
+                        value={formik.values.password} 
+                        margin="normal" 
+                    />
+                    {formik.errors.password && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+                    <FormControlLabel 
+                        label={'Remember me'} 
+                        control={
+                            <Checkbox  
+                                name='rememberMe'
+                                onChange={formik.handleChange}
+                                checked={formik.values.rememberMe}
+                        />
+                        } 
+                    />
+                    <Button 
+                        type={'submit'} 
+                        variant={'contained'} 
+                        color={'primary'}
+                    >
+                        Login
+                    </Button>
+                    </FormGroup>
+                </FormControl>
+            </form>
         </Grid>
     </Grid>
     )
